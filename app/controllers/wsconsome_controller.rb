6eec8be2756219b8d2c93987b273f8fb292b1f2e
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-12-31 01:03:01 -0200

    teste

--- app/controllers/wsconsome_controller.rb
+++ app/controllers/wsconsome_controller.rb
@@ -2,13 +2,13 @@ require 'soap/wsdlDriver'
 
 class WsconsomeController < ApplicationController
 
-#	WSDL = "http://workflow.ghc.com.br/wsghcworkflow/WSGHCWorkflow.asmx?WSDL"
+	WSDL = "http://workflow.ghc.com.br/wsghcworkflow/WSGHCWorkflow.asmx?WSDL"
 #	WSDL = "http://130.158.1.41/WSGHCWorkflow/WSGHCWorkflow.asmx?WSDL"
 
 	def index
  		soap = wsdl.create_rpc_driver
 #		soap ||= SOAP::WSDLDriverFactory.new(WSDL).create_rpc_driver
-		response = soap.GHCGenericFunction(:id => '11', :param => 'param^param')
+		response = soap.GHCGenericFunction(:id => '10', :param => 'param^param')
 		soap.reset_stream
 		render :text=> response.gHCGenericFunctionResult
 	end
@@ -16,7 +16,7 @@ class WsconsomeController < ApplicationController
 
  private
     def wsdl
-      SOAP::WSDLDriverFactory.new("http://130.158.1.41/WSGHCWorkflow/WSGHCWorkflow.asmx?WSDL")
+      SOAP::WSDLDriverFactory.new(WSDL)
     end
 
 end
